5647617fa455be3afcea2198bf1a453d0c88bc16
marta1014 <lovemadenghui@126.com>
2020-02-21 15:40:22 +0800

    整体基础架子搭建

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,223 +1,174 @@
 <template>
-  <view class="content" :style="{'height':windowHighly}">
-
-    <search @sendHeight="getHeight"/>
-
-   <swiper indicator-dots class="swiper" autoplay>
-	  <swiper-item class="item">
-		  <navigator src="" class="navigator">
-			  <image src="https://marta1014.github.io/vx-pro1//static/uploads/banner1.png"></image>
-		  </navigator>
-	  </swiper-item>
-	  <swiper-item class="item">
-		  <navigator src="" class="navigator">
-			  <image src="https://marta1014.github.io/vx-pro1//static/uploads/banner2.png"></image>
-		  </navigator>
-	  </swiper-item>
-	  <swiper-item class="item">
-		   <navigator src="" class="navigator">
-			  <image src="https://marta1014.github.io/vx-pro1//static/uploads/banner3.png"></image>
-		  </navigator>
-	  </swiper-item>
-	  </swiper>
-  <!-- banner区 / -->
-
-  <view class="nav">
-	  <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/icon_index_nav_1@2x.png"></image>
-	  </navigator>
-	   <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/icon_index_nav_2@2x.png"></image>
-	  </navigator>
-	   <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/icon_index_nav_3@2x.png"></image>
-	  </navigator>
-	   <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/icon_index_nav_4@2x.png"></image>
-	  </navigator>
-  </view>
-  <!-- nav区 /-->
-
-  <view class="floor">
-	      <view class="title">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_title.png"/>
-	      </view>
-          <view class="imgs">
-			<view class="left">
-		  <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_1@2x.png"/>
-	      </navigator>
-		  </view>
-		  <view class="right">
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_2@2x.png"/>
-	      </navigator>
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_3@2x.png"/>
-	      </navigator>
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_4@2x.png"/>
-	      </navigator>
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_5@2x.png"/>
-	      </navigator>
-		  </view>
-          </view>
-  </view>
-  <!-- <view class="floor">
-	      <view class="title">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_title.png"/>
-	      </view>
-          <view class="imgs">
-			<view class="left">
-		  <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_1@2x.png"/>
-	      </navigator>
-		  </view>
-		  <view class="right">
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_2@2x.png"/>
-	      </navigator>
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_3@2x.png"/>
-	      </navigator>
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_4@2x.png"/>
-	      </navigator>
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_5@2x.png"/>
-	      </navigator>
-		  </view>
-          </view>
-  </view> -->
-  <!-- <view class="floor">
-	      <view class="title">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_title.png"/>
-	      </view>
-          <view class="imgs">
-			<view class="left">
-		  <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_1@2x.png"/>
-	      </navigator>
-		  </view>
-		  <view class="right">
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_2@2x.png"/>
-	      </navigator>
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_3@2x.png"/>
-	      </navigator>
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_4@2x.png"/>
-	      </navigator>
-	      <navigator class="item">
-		  <image src="https://marta1014.github.io/vx-pro1//static/uploads/pic_floor02_5@2x.png"/>
-	      </navigator>
-		  </view>
-          </view>
-  </view> -->
+  <view :style="{height: pageHeight, overflow: 'hidden'}">
+    <search @search="disableScroll" />
+    <!-- 焦点图 -->
+    <swiper class="banner" indicator-dots indicator-color="rgba(255, 255, 255, 0.6)" indicator-active-color="#fff">
+      <swiper-item>
+        <navigator url="/pages/goods/index">
+          <image src="http://static.botue.com/ugo/uploads/banner1.png"></image>
+        </navigator>
+      </swiper-item>
+      <swiper-item>
+        <navigator url="/pages/goods/index">
+          <image src="http://static.botue.com/ugo/uploads/banner2.png"></image>
+        </navigator>
+      </swiper-item>
+      <swiper-item>
+        <navigator url="/pages/goods/index">
+          <image src="http://static.botue.com/ugo/uploads/banner3.png"></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+    <!-- 导航条 -->
+    <view class="navs">
+      <navigator open-type="switchTab" url="/pages/category/index">
+        <image src="http://static.botue.com/ugo/uploads/icon_index_nav_4@2x.png"></image>
+      </navigator>
+      <navigator url="/pages/list/index">
+        <image src="http://static.botue.com/ugo/uploads/icon_index_nav_3@2x.png"></image>
+      </navigator>
+      <navigator url="/pages/list/index">
+        <image src="http://static.botue.com/ugo/uploads/icon_index_nav_2@2x.png"></image>
+      </navigator>
+      <navigator url="/pages/list/index">
+        <image src="http://static.botue.com/ugo/uploads/icon_index_nav_1@2x.png"></image>
+      </navigator>
+    </view>
+    <!-- 楼层 -->
+    <view class="floors">
+      <view class="floor">
+        <view class="title">
+          <image src="http://static.botue.com/ugo/uploads/pic_floor01_title.png"></image>
+        </view>
+        <view class="items">
+          <navigator url="/pages/list/index">
+            <image src="http://static.botue.com/ugo/uploads/pic_floor01_1@2x.png"></image>
+          </navigator>
+          <navigator url="/pages/list/index">
+            <image src="http://static.botue.com/ugo/uploads/pic_floor01_2@2x.png"></image>
+          </navigator>
+          <navigator url="/pages/list/index">
+            <image src="http://static.botue.com/ugo/uploads/pic_floor01_3@2x.png"></image>
+          </navigator>
+          <navigator url="/pages/list/index">
+            <image src="http://static.botue.com/ugo/uploads/pic_floor01_4@2x.png"></image>
+          </navigator>
+          <navigator url="/pages/list/index">
+            <image src="http://static.botue.com/ugo/uploads/pic_floor01_5@2x.png"></image>
+          </navigator>
+        </view>
+      </view>
+    </view>
+    <!-- 回到顶部 -->
+    <view class="goTop icon-top"></view>
   </view>
 </template>
 
 <script>
-import search from '@/components/search'
-export default {
-  components:{search},
-  data() {
-    return {
-      windowHighly:"native"
-    };
-  },
-  methods:{
-    getHeight(height){
-		//   设置样式页面高度为接收值
-		  this.windowHighly = `${height}rpx`
-	  }
+  import search from '@/components/search';
+
+  export default {
+
+    data () {
+      return {
+        pageHeight: 'auto'
+      }
+    },
+
+    components: {
+      search
+    },
+    
+    methods: {
+      disableScroll (ev) {
+        this.pageHeight = ev.pageHeight + 'px';
+      }
+    }
   }
-};
 </script>
 
-<style lang="less">
-.content{
-  overflow: hidden;
-  .swiper {//banner区
-		height: 340rpx;
-			.navigator{
-				height: 100%;
-				image{
-				height: 100%;
-				width: 100%;
-			}
-			}
-  }
-  .nav {//nav区域
-		display: flex;
-		margin-bottom: 4rpx;
-		.item {
-			flex: 1;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			width: 128rpx;
-			height: 140rpx;
-			image{
-				width: 100%;
-				height: 100%;
-			}
-		}
-  }
-  .floor{
-    .title{
-			padding-top: 16rpx;
-			padding-left: 20rpx;
-			height: 60rpx;
-			box-sizing: border-box;
-			image{
-				height: 100%;
-				width: 100%;
-				// vertical-align: middle;
-			}
+<style scoped lang="less">
+  .banner {
+    width: 100%;
+    height: 340rpx;
+
+    image {
+      width: 100%;
+      height: 340rpx;
     }
-    .imgs{
-      box-sizing: border-box;
-		padding: 20rpx 16rpx;
+  }
+
+  .navs {
     display: flex;
-    .left{
-			margin-right: 4rpx;
-			image{
-			width: 232rpx;
-			height: 386rpx;
-			vertical-align: middle;
-			}
-    }
-    .right{
-      overflow: hidden;
-      height: 386rpx;
-      .item {
-				float: left;
-			}
-			:nth-child(1),:nth-child(3){
-				margin-right: 4rpx;
-			}
-			:nth-child(3),:nth-child(4){
-				margin-top: 10rpx;
-			}
-			:nth-child(1),:nth-child(4){
-				image{
-				width: 272rpx;
-				height: 188rpx;
-				vertical-align: middle;
-				}
-      }
-      :nth-child(2),:nth-child(3){
-				image{
-				width: 202rpx;
-				height: 188rpx;
-				vertical-align: middle;
-				}
-			}
-    }
+    justify-content: space-between;
+    padding: 30rpx 44rpx;
+
+    image {
+      width: 128rpx;
+      height: 140rpx;
     }
   }
-}
+
+  .floor {
+
+    .title {
+      width: 750rpx;
+      height: 60rpx;
+      padding: 20rpx 0 0 8rpx;
+      background-color: #f4f4f4;
+    }
+
+    .items {
+      padding: 20rpx 16rpx;
+      overflow: hidden;
+
+      navigator {
+        width: 193rpx;
+        height: 188rpx;
+        margin-left: 10rpx;
+        margin-bottom: 10rpx;
+        float: left;
+      }
+
+      navigator:first-child {
+        width: 232rpx;
+        height: 386rpx;
+        margin-left: 0rpx;
+      }
+
+      navigator:nth-child(2),
+      navigator:nth-child(5) {
+        width: 273rpx;
+      }
+    }
+
+    &:first-child {
+
+      .items {
+
+        navigator {
+          width: 233rpx;
+        }
+      }
+    }
+  }
+
+  .goTop {
+    position: fixed;
+    bottom: 30rpx;
+    /* #ifdef H5 */
+    bottom: 65px;
+    /* #endif */
+    right: 30rpx;
+  
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100rpx;
+    height: 100rpx;
+    font-size: 48rpx;
+    color: #666;
+    border-radius: 50%;
+    background-color: rgba(255, 255, 255, 0.8);
+  }
 </style>
